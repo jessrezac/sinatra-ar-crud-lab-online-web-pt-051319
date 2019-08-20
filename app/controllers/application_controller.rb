a9bd671486604174cdb7fa73b45e806b4455783e
Jess Rezac <jess.rezac@gmail.com>
2019-08-20 18:01:14 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,13 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to "/articles"
+  end
 
+  get '/articles' do
+    @articles = Article.all
+
+    erb :index
   end
 
   get '/articles/new' do
