98761d2acf4cc5ac172a9e44b8fed11c6213f4ee
Jess Rezac <jess.rezac@gmail.com>
2019-08-20 18:20:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    redirect to '/articles'
+    redirect to "/articles"
   end
 
   get '/articles' do
@@ -25,7 +25,7 @@ class ApplicationController < Sinatra::Base
   post '/articles' do
     @article = Article.create(title: params[:title], content: params[:content])
 
-    redirect to '/articles/#{@article.id}'
+    redirect to "/articles/#{@article.id}"
   end
 
   get '/articles/:id' do
