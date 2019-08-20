cc8fa9181c2196d710675f1058ace2301da18c26
Jess Rezac <jess.rezac@gmail.com>
2019-08-20 18:33:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,8 +40,7 @@ class ApplicationController < Sinatra::Base
   end
 
   patch '/articles/:id' do
-    binding.pry
-    article = article.find(params[:id])
+    article = Article.find(params[:id])
     article.title = params[:title]
     article.content = params[:content]
     article.save
